cc74607492e7e07f91edc4a0a4596cc36774a136
deverte <deverte@ya.ru>
2020-07-12 17:49:03 +0700

    poetry-orange-widget updated to v0.2

--- cookiecutters/poetry-orange-widget/README.md
+++ cookiecutters/poetry-orange-widget/README.md
@@ -1,6 +1,6 @@
 # poetry-orange-widget
 
-**version: 0.1**
+**version: 0.2**
 
 ---
 
--- cookiecutters/poetry-orange-widget/{{cookiecutter.widget_name}}/{{cookiecutter.widget_name}}/{{cookiecutter.widget_name}}.py
+++ cookiecutters/poetry-orange-widget/{{cookiecutter.widget_name}}/{{cookiecutter.widget_name}}/{{cookiecutter.widget_name}}.py
@@ -1,3 +1,12 @@
+"""{{cookiecutter.widget_name}} Widget
+
+Description.
+
+See:
+    - https://orange-development.readthedocs.io/
+    - https://orange3.readthedocs.io/projects/orange-development/_modules/orangewidget/widget.html
+"""
+
 from Orange.widgets.widget import OWWidget
 from Orange.widgets.widget import Input
 from Orange.widgets.widget import Output
@@ -9,7 +18,7 @@ from AnyQt.QtGui import QDoubleValidator
 
 
 class {{cookiecutter.widget_name}}(OWWidget):
-    """Docstring"""
+    """Docstring."""
 
     # -------------------------------------------------------------------------
     # Widget info
@@ -56,7 +65,7 @@ class {{cookiecutter.widget_name}}(OWWidget):
         # --------------------
         gui.lineEdit(
             self.controlArea, self, 'some_setting', 'label', callback=self.some_setting_changed,
-            box='Base Settings', valueType=float, validator=QDoubleValidator())
+            box='Base Settings', valueType=float)
 
         # --------------------
         # Callbacks (on init)
@@ -68,19 +77,13 @@ class {{cookiecutter.widget_name}}(OWWidget):
     # Transformations
     # -------------------------------------------------------------------------
     def some_transformation(self):
-        """Docstring"""
+        """Docstring."""
 
-        # --------------------
-        # Data transformations
-        # --------------------
         out_data = self.in_data
         if self.some_setting % 2 == 1:
             out_data.shuffle()
 
-        # --------------------
-        # Outputs
-        # --------------------
-        self.Outputs.out_data.send(out_data)
+        return out_data
 
 
     # -------------------------------------------------------------------------
@@ -88,13 +91,25 @@ class {{cookiecutter.widget_name}}(OWWidget):
     # -------------------------------------------------------------------------
     @Inputs.in_data
     def set_data(self, in_data):
-        """Recieves and validates the input data."""
+        """Recieves, validates and process (if needed) the input data."""
 
         self.in_data = in_data
         self.some_transformation()
 
 
-    def some_setting_changed(self):
-        """Docstring"""
+    def handleNewSignals(self):
+        """
+        Invoked by the workflow signal propagation manager after all
+        signals handlers have been called.
+        Reimplement this method in order to coalesce updates from
+        multiple updated inputs.
+        """
 
-        self.some_transformation()
+        pass
+
+
+    def some_setting_changed(self):
+        """Docstring."""
+
+        out_data = self.some_transformation()
+        self.Outputs.out_data.send(out_data)
